5b141d189128ede3cc26c6711898ed1e928e6351
Harshanan Saddanathan <hsaddanathan21@gmail.com>
2020-10-14 17:57:44 -0400

    Got current weather working completely

--- script.js
+++ script.js
@@ -21,18 +21,32 @@ $(document).ready(function(){
 
 //Get Weather Data
 function currentWeather(cityName){
-    var queryUrl= "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
+    var queryUrl= "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey + "&units=imperial";
 
     $.ajax({
         url: queryUrl,
         method: "GET"
     }).done(function(response){
         console.log(response);
-        $("#city-name").text(""+response.name+" " + moment().format("L")).append($('<img class="images" src ="https://openweathermap.org/img/wn/' + response.weather[0].icon + '.png"/>'));
+        $("#city-name").text(""+response.name+" (" + moment().format("L") + ")").append($('<img class="images" src ="https://openweathermap.org/img/wn/' + response.weather[0].icon + '.png"/>'));
+        $("#temp").text("Temperature: " + Math.ceil(response.main.temp)+ String.fromCharCode(176) + "F");
+        $("#humid").text("Humidity: " + response.main.humidity + "%");
+        $("#wind-speed").text("Wind Speed: " + response.wind.speed + " MPH");
+        getUVIndex(response.coord.lat, response.coord.lon);
     })
 }
 
-
+function getUVIndex(lat,lon){
+    var queryUrl= "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon  + "&appid=" + apiKey + "&units=imperial";
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    }).then(function(response){
+        console.log(response)
+        var uV = response.current.uvi;
+        $("#uv-index").text("UV Index: " + uV);
+    })
+}
 
 
 
